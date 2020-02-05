8ee6a48e108c4bce76f3228ae5d1a10d493c26b4
kk-ruby <k.karasawa@kageyama-co.jp>
2020-02-05 09:28:57 +0900

    20200205_helper変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,41 +4,15 @@ class HomeController < ApplicationController
   end
 
   def shuffle
-    @group1 = Emplo.where(section: "製造").sort_by{rand}
-    @n = Emplo.where(section: "製造").count.to_i
-    @n2 = 0
+    @group1 = Emplo.where(section: "事務").count.to_i
 
-    if (@n % 3) == 0
-      @n2 = (@n / 3)
-    else
-      @n2 = (@n / 3) + 1
-    end
-
-    @aaa = Array.new()
-    numnum = 0 
-
-    @group1.each do |f|
-     @aaa[numnum] = f.name
-     numnum += 1
-    end
-
-    @group2 = Emplo.where(section: "事務").sort_by{rand}
-    @m = Emplo.where(section: "事務").count.to_i
-    @m2 = 0
-
-    if (@m % 3) == 0
-      @m2 = (@m / 3)
-    else
-      @m2 = (@m / 3) + 1
-    end
-
-    @bbb = Array.new()
-    numnum2 = 0 
-
-    @group2.each do |f|
-     @bbb[numnum2] = f.name
-     numnum2 += 1
+    for num in range1 do
+      p num[] <<
     end
 
   end
+
+
+
+
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,5 @@
 module HomeHelper
+
+    aaaaa
+
 end
